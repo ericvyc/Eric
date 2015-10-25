a7333c0c48dbbc96a5c0588b493d64764999b097
eric.castanheira <eric.castanheira@192.168.182.34>
2015-10-25 02:48:55 -0200

    Aula de panel grid, input text, output label, facet e ação jsf.

---
+++ src/main/java/curso/javaee/primefaces/CadastroAlunoBean.java
@@ -0,0 +1,35 @@
+package curso.javaee.primefaces;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class CadastroAlunoBean implements Serializable{
+
+	private static final long serialVersionUID = -4337546447689221263L;
+
+	private String nome;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void cadastrar(){
+		System.out.println("Nome : " + getNome());
+		System.out.println("Email : "+ getEmail());
+	}
+	
+}
