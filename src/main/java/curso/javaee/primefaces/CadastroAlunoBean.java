bbc2106732bf421f329d4412e4e72d37a6d9ad3f
eric.castanheira <eric.castanheira@192.168.182.34>
2015-10-25 02:51:27 -0200

    Identação

--- src/main/java/curso/javaee/primefaces/CadastroAlunoBean.java
+++ src/main/java/curso/javaee/primefaces/CadastroAlunoBean.java
@@ -12,17 +12,21 @@ public class CadastroAlunoBean implements Serializable{
 	private static final long serialVersionUID = -4337546447689221263L;
 
 	private String nome;
+	
 	private String email;
 	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
